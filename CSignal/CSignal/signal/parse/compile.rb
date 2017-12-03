b8868612d1c26d887956fee68b049890ecbdbb8a
desktopgame <koya0306@outlook.jp>
2017-12-03 15:59:26 +0900

    コンパイル用のスクリプトを追加

---
+++ CSignal/CSignal/signal/parse/compile.rb
@@ -0,0 +1,28 @@
+require 'open3'
+
+o, e, s = Open3.capture3("flex_compile.bat")
+o = o.delete("\"");
+o.each_line {|line| p line.chomp }
+
+begin
+	file = File.open("signal.tab.h","r")
+	buffer = ""
+	file.each do |text|
+		buffer += (text)
+	end
+	#puts buffer
+	file.close
+
+	DQ = "\"";
+	includeTargets = ["../ast/ast.h"]
+	prepend = "//insert by compile.rb\n"
+	includeTargets.each do |include|
+		prepend += ("#include " + DQ + include + DQ + "\n")
+	end
+	buffer = prepend + buffer;
+	File.open("signal.tab.h", "w") do |f| 
+		f.puts(buffer)
+	end
+rescue => e
+	p e
+end
