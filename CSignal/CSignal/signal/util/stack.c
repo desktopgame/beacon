8f332541cf9db43a18afbb2297aba76bf594a93a
desktopgame <koya0306@outlook.jp>
2017-12-03 10:58:55 +0900

    スタックを追加

---
+++ CSignal/CSignal/signal/util/stack.c
@@ -0,0 +1,91 @@
+#include "stack.h"
+#include <assert.h>
+#include <stdlib.h>
+
+stack * stack_new() {
+	stack* st = (stack*)malloc(sizeof(stack));
+	st->item = NULL;
+	st->prev = NULL;
+	st->next = NULL;
+	return st;
+}
+
+void stack_push(stack * self, stack_item item) {
+	assert(self != NULL);
+	assert(item != NULL);
+	if (self->item == NULL) {
+		self->item = item;
+		return;
+	}
+	if (self->next == NULL) {
+		stack* nextElement = stack_new();
+		self->next = nextElement;
+		nextElement->prev = self;
+		nextElement->item = item;
+	} else {
+		stack_push(self->next, item);
+	}
+}
+
+stack_item stack_top(stack * self) {
+	assert(self != NULL);
+	if (self->next == NULL) {
+		return self->item;
+	} else {
+		return stack_top(self->next);
+	}
+}
+
+stack_item stack_pop(stack * self) {
+	assert(self != NULL);
+	if (self->next == NULL) {
+		if (self->prev == NULL) {
+			stack_item ret = self->item;
+			self->item = NULL;
+			return ret;
+		} else {
+			stack* prevElement = self->prev;
+			prevElement->next = NULL;
+			self->prev = NULL;
+			stack_item ret = self->item;
+			free(self);
+			return ret;
+		}
+	} else {
+		return stack_pop(self->next);
+	}
+}
+
+bool stack_empty(stack * self) {
+	assert(self != NULL);
+	return (self->item == NULL);
+}
+
+void stack_delete(stack * self, stack_element_deleter deleter) {
+	assert(self != NULL);
+	stack* pointee = self;
+	while (1) {
+		stack* next = pointee->next;
+		pointee->next = NULL;
+		if (next) {
+			next->prev = NULL;
+		}
+		stack_item item = pointee->item;
+		pointee->item = NULL;
+		if (item) {
+			deleter(item);
+		}
+		free(pointee);
+		pointee = next;
+		if (!next) {
+			break;
+		}
+	}
+}
+
+void stack_deleter_free(stack_item item) {
+	free(item);
+}
+
+void stack_deleter_null(stack_item item) {
+}
---
+++ CSignal/CSignal/signal/util/stack.h
@@ -0,0 +1,85 @@
+#pragma once
+#ifndef SIGNAL_UTIL_STACK_H
+#define SIGNAL_UTIL_STACK_H
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * スタックに格納出来るデータの型.
+ */
+typedef void* stack_item;
+
+/**
+ * スタックを表す構造体.
+ */
+typedef struct stack {
+	stack_item item;
+	struct stack* prev;
+	struct stack* next;
+} stack;
+
+/**
+ * スタックのデリータ関数です.
+ * @param item
+ */
+typedef void(*stack_element_deleter)(stack_item item);
+
+/**
+ * 新しいスタックを作成します.
+ * @return
+ */
+stack* stack_new(void);
+
+/**
+ * 末尾へ値を追加します.
+ * @param self
+ * @param item
+ */
+void stack_push(stack* self, stack_item item);
+
+/**
+ * 末尾の値を返しましす.
+ * @param self
+ * @return
+ */
+stack_item stack_top(stack* self);
+
+/**
+ * 末尾の値を返して削除します.
+ * @param self
+ * @return
+ */
+stack_item stack_pop(stack* self);
+
+/**
+ * スタックが空なら true.
+ * @param self
+ * @return
+ */
+bool stack_empty(stack* self);
+
+/**
+ * スタックとその中身を開放します.
+ * @param self
+ * @param deleter
+ */
+void stack_delete(stack* self, stack_element_deleter deleter);
+
+/**
+ * free によって解放するデリータです.
+ * @param item
+ */
+void stack_deleter_free(stack_item item);
+
+/**
+ * 何も行わないデリータです.
+ * @param item
+ */
+void stack_deleter_null(stack_item item);
+#ifdef __cplusplus
+};
+#endif
+#endif // !SIGNAL_UTIL_STACK_H
