829968442c603053df4b3813c32a5c740a5f26bb
desktopgame <koya0306@outlook.jp>
2017-12-03 10:59:16 +0900

    ファイル読み込み関数を追加

---
+++ CSignal/CSignal/signal/util/io.c
@@ -0,0 +1,44 @@
+#include "io.h"
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char * io_read_text(const char * filename) {
+	FILE* fp;
+	errno_t err = fopen_s(&fp, filename, "r");
+	if (err) {
+		//error
+		fclose(fp);
+		return NULL;
+	}
+	int buffer_size = 16;
+	int length = 0;
+	char* ret = (char*)malloc(sizeof(char) * buffer_size);
+	while (1) {
+		char c = fgetc(fp);
+		if (c == EOF) {
+			break;
+		}
+		ret[length] = c;
+		length++;
+		//予め確保していた領域をはみ出した
+		if (length >= buffer_size) {
+			buffer_size = buffer_size + (buffer_size / 2);
+			char* temp = (char*)realloc(ret, sizeof(char) * buffer_size);
+			//FIXME:再確保失敗時
+			ret = temp;
+		}
+	}
+	//多めに確保しすぎた
+	if (length < buffer_size) {
+		char* copy = (char*)malloc(sizeof(char) * length);
+		for (int i = 0; i < length; i++) {
+			copy[i] = ret[i];
+		}
+		free(ret);
+		ret = copy;
+	}
+	fclose(fp);
+	return ret;
+}
---
+++ CSignal/CSignal/signal/util/io.h
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef SIGNAL_UTIL_IO_H
+#define SIGNAL_UTIL_IO_H
+
+/**
+ * 指定のファイルを読み込んで返します.
+ * @param filename
+ * @return
+ */
+char* io_read_text(const char* filename);
+#endif // !SIGNAL_UTIL_IO_H
