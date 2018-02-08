1c6667707983abddd881d69d0c76f6047e92034e
desktopgame <koya0306@outlook.jp>
2018-02-08 12:25:44 +0900

    doubleをコンストラクタから構築できるように

--- CSignal/CSignal/signal/lib/signal/lang/sg_double.c
+++ CSignal/CSignal/signal/lib/signal/lang/sg_double.c
@@ -1,8 +1,20 @@
 #include "sg_double.h"
 #include "../../sg_library_impl.h"
 
+//proto
+static void sg_double_nativeInit(method* parent, vm* vmc, enviroment* env);
+
 void sg_double_init() {
 	namespace_* lang = namespace_lang();
 	class_* doubleClass = class_new_preload("Double");
 	namespace_add_type(lang, type_wrap_class(doubleClass));
+	class_define_native_method(doubleClass, "nativeInit", sg_double_nativeInit);
+}
+//private
+static void sg_double_nativeInit(method* parent, vm* vmc, enviroment* env) {
+	object* d = vector_pop(vmc->value_stack);
+	object* self = vector_at(vmc->ref_stack, 0);
+
+	self->u.double_ = d->u.double_;
+	self->type = object_double;
 }
