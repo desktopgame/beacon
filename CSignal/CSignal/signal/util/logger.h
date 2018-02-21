cf91d8d7a61e17b12a7575921e442d3872cf4aeb
desktopgame <koya0306@outlook.jp>
2018-02-21 10:25:38 +0900

    loggerのいらないマクロ定義を削除

--- CSignal/CSignal/signal/util/logger.h
+++ CSignal/CSignal/signal/util/logger.h
@@ -59,48 +59,11 @@ void logger_fatal(const char* filename, int lineno, const char* source, ...);
  */
 void logger_test(bool cond);
 
-//マクロ名が被る場合は LOG_OVERRIDE を定義して既存のものを無効にします。
-//(割と一般的な名前を使用しているので用意してます。)
-#if defined(LOG_OVERRIDE)
-#if defined(LOG)
-#undef LOG
-#endif
-
-#if defined(INFO)
-#undef INFO
-#endif
-
-#if defined(WARN)
-#undef WARN
-#endif
-
-#if defined(ERROR)
-#undef ERROR
-#endif
-
-#if defined(FATAL)
-#undef FATAL
-#endif
-#endif
 #define TEST(cond) logger_test(cond)
-//ログ出力用マクロを無効にするには
-//LOG_DISABLEを定義します。
-#if !defined(LOG_DISABLE)
-#if defined(__FILE__) && defined(__LINE__)
-
 #define DEBUG(source) logger_log(log_debug, __FILE__, __LINE__, source)
 #define INFO(source) logger_log(log_info, __FILE__, __LINE__, source)
 #define WARN(source) logger_log(log_warn, __FILE__, __LINE__, source)
 #define ERROR(source) logger_log(log_error, __FILE__, __LINE__, source)
 #define FATAL(source) logger_log(log_fatal, __FILE__, __LINE__, source)
 
-#else
-#define DEBUG(source) sg_log(log_debug, "invalid", -1, source)
-#define INFO(source) sg_log(log_info, "invalid", -1, source)
-#define WARN(source) sg_log(log_warn, "invalid", -1, source)
-#define ERROR(source) sg_log(log_error, "invalid", -1, source)
-#define FATAL(source) sg_log(log_fatal, "invalid", -1, source)
-
-#endif
-#endif
 #endif // !SIGNAL_UTIL_LOGGER_H
