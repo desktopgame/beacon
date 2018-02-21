8fcfde7c58ac50da25d693f00d06b014cdefe656
desktopgame <koya0306@outlook.jp>
2018-02-21 19:59:52 +0900

    自動終了の機能を追加

--- CSignal/CSignal/signal/parse/compile.rb
+++ CSignal/CSignal/signal/parse/compile.rb
@@ -5,3 +5,19 @@ require_relative 'ruby_lib/build'
 Make.lex()
 Make.yy()
 Build.compile()
+
+#0.9秒後にクローズ
+times = 0.0
+frame = 0.1
+length = 0.9
+waiter = Thread.new do
+    while(times < length)
+        sleep(frame)
+        times += frame
+    end
+    exit
+end
+#入力されたら即座に終了
+gets()
+times = length
+waiter.join()
