5b5d88aaff946ef3708e830f19fe9f44ec01a4bb
Koya <koya0306@outlook.jp>
2018-04-23 12:07:10 +0900

    使用されていない alias.h を削除

--- beacon/src/beacon/util/alias.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef BEACON_UTIL_ALIAS_H
-#define BEACON_UTIL_ALIAS_H
-#include <stdint.h>
-
-typedef char SG_char;
-typedef int SG_int;
-typedef double SG_double;
-#endif // !SIGNAL_UTIL_ALIAS_H
