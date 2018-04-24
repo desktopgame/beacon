248588728a39b05f9e17d6c5dcf7517dbca3e66c
Koya <koya0306@outlook.jp>
2018-04-24 15:37:17 +0900

    インポート解決時の拡張子を .bc に

--- beacon/src/beacon/env/cll/class_loader_bcload_import_module_impl.c
+++ beacon/src/beacon/env/cll/class_loader_bcload_import_module_impl.c
@@ -45,7 +45,7 @@ static void CLBC_import_internal(class_loader* self, vector* ilimports, int i) {
 	}
 	vector_item e = vector_at(ilimports, i);
 	il_import* import = (il_import*)e;
-	char* withExt = text_concat(import->path, ".signal");
+	char* withExt = text_concat(import->path, ".bc");
 	char* fullPath = io_absolute_path(withExt);
 	CLBC_new_load(self, fullPath);
 	MEM_FREE(withExt);
