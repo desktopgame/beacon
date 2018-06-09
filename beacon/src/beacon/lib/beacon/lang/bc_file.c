c3af17af6c20549793af3fb4ede5c4b53b12092a
Koya <koya0306@outlook.jp>
2018-06-10 00:33:03 +0900

    File.openの動作だけ確認

--- beacon/src/beacon/lib/beacon/lang/bc_file.c
+++ beacon/src/beacon/lib/beacon/lang/bc_file.c
@@ -1,6 +1,10 @@
 #include "bc_file.h"
 #include "../../../env/namespace.h"
 #include "../../../env/type_impl.h"
+#include "../../../util/string_buffer.h"
+#include "../../../util/text.h"
+#include "../../bc_library_impl.h"
+#include "bc_string.h"
 
 static void bc_file_nativeOpen(method* parent, frame* fr, enviroment* env);
 static void bc_file_nativePut(method* parent, frame* fr, enviroment* env);
@@ -12,15 +16,15 @@ static void bc_file_nativeGetClose(method* parent, frame* fr, enviroment* env);
 
 void bc_file_init() {
 	namespace_* unsafe = namespace_unsafe();
-	type* arrayType = class_new_preload("File");
-	class_* arrayClass = TYPE2CLASS(arrayType);
-	namespace_add_type(unsafe, arrayType);
-	class_define_native_method(arrayClass, "nativeOpen", bc_file_nativeOpen);
-	class_define_native_method(arrayClass, "nativePut", bc_file_nativePut);
-	class_define_native_method(arrayClass, "nativeGet", bc_file_nativeGet);
-	class_define_native_method(arrayClass, "nativeStdIn", bc_file_nativeGetStdIn);
-	class_define_native_method(arrayClass, "nativeStdOut", bc_file_nativeGetStdOut);
-	class_define_native_method(arrayClass, "nativeStdErr", bc_file_nativeGetStdErr);
+	type* fileType = class_new_preload("File");
+	class_* fileClass = TYPE2CLASS(fileType);
+	namespace_add_type(unsafe, fileType);
+	class_define_native_method(fileClass, "nativeOpen", bc_file_nativeOpen);
+	class_define_native_method(fileClass, "nativePut", bc_file_nativePut);
+	class_define_native_method(fileClass, "nativeGet", bc_file_nativeGet);
+	class_define_native_method(fileClass, "nativeStdIn", bc_file_nativeGetStdIn);
+	class_define_native_method(fileClass, "nativeStdOut", bc_file_nativeGetStdOut);
+	class_define_native_method(fileClass, "nativeStdErr", bc_file_nativeGetStdErr);
 }
 
 type* bc_file_type() {
@@ -29,6 +33,12 @@ type* bc_file_type() {
 }
 //private
 static void bc_file_nativeOpen(method* parent, frame* fr, enviroment* env) {
+	object* fileObj = vector_at(fr->ref_stack, 1);
+	object* modeObj = vector_at(fr->ref_stack, 2);
+	string_buffer* fileStr = bc_string_raw(fileObj);
+	string_buffer* modeStr = bc_string_raw(modeObj);
+	text_printfln("%s : %s", fileStr->text, modeStr->text);
+	int a = 0;
 }
 
 static void bc_file_nativePut(method* parent, frame* fr, enviroment* env) {
