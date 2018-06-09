26084d8d1aeae6ec1a5a6ed54810bb39721a79cb
Koya <koya0306@outlook.jp>
2018-06-09 09:32:20 +0900

    長すぎる列をダンプするスクリプト追加

---
+++ beacon/rbtools/rcolumncheck.rb
@@ -0,0 +1,46 @@
+require './rgrep'
+p "target: " + File.expand_path('./../src')
+file_list = []
+line_dict = {}
+class LineData
+	attr_accessor :line, :length
+	def initialize(aline, alength)
+		@line = aline
+		@length = alength
+	end
+end
+Dir.glob(File.expand_path('./../src/**/*')) do | filename |
+	if(!filename.end_with?(".h")) then
+		next
+	end
+	File.open(filename) do | file |
+		content = file.read()
+		content.lines {|line|
+			line = line.lstrip().rstrip()
+			if(line.length() >= 80) then
+				file_list << filename
+				if(!line_dict.has_key?(filename)) then
+					line_dict[filename] = []
+				end
+				line_list = line_dict[filename]
+				line_list << LineData.new(sprintf("<%d>%s", line.length(), line), line.length())
+			end
+		}
+	end
+end
+file_list.each do | filename |
+	list = line_dict[filename]
+	list.sort! do |a, b|
+		b.length <=> a.length
+	end
+end
+File.open("rcolumn.text", "w") do |file|
+	file_list.each do | filename |
+		file.puts(filename)
+		list = line_dict[filename]
+		list.each do | data |
+			file.write("    ")
+			file.puts(data.line)
+		end
+	end
+end
---
+++ beacon/rbtools/readme.md
@@ -0,0 +1,7 @@
+# rbtools
+ソースコードの解析や自動生成のためのツール  
+rbtoolsがカレントディレクトリでないと動作しません。  
+````
+cd rbtools
+ruby hogehoge.rb
+````
