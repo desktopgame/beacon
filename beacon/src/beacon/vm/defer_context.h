a3c3622d1c8d534d6b69d659bc48589eec933f71
Koya <koya0306@outlook.jp>
2018-06-17 00:06:27 +0900

    コメント追加

--- beacon/src/beacon/vm/defer_context.h
+++ beacon/src/beacon/vm/defer_context.h
@@ -2,12 +2,24 @@
 #define BEACON_VM_DEFER_CONTEXT_H
 #include "label.h"
 #include "../util/vector.h"
+/**
+ * deferステートメントが実行された時、
+ * その時点でのpcと名前束縛を記録しておく構造体です.
+ */
 typedef struct defer_context {
 	label* offset;
 	vector* bind;
 } defer_context;
 
+/**
+ * 新しい defer_context を作成します.
+ * @return
+ */
 defer_context* defer_context_new();
 
+/**
+ * defer_context を解放します.
+ * @param self
+ */
 void defer_context_delete(defer_context* self);
 #endif
