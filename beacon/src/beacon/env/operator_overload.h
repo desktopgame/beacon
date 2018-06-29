ad6fedead77160487d2141142b442d0764e2bab5
Koya <koya0306@outlook.jp>
2018-06-29 23:45:25 +0900

    コメントを追加

--- beacon/src/beacon/env/operator_overload.h
+++ beacon/src/beacon/env/operator_overload.h
@@ -7,6 +7,9 @@ struct type;
 struct enviroment;
 struct generic_type;
 struct frame;
+/**
+ * 演算子オーバーロードを表す構造体.
+ */
 typedef struct operator_overload {
 	struct type* parent;
 	struct generic_type* return_gtype;
@@ -16,9 +19,24 @@ typedef struct operator_overload {
 	access_level access;
 } operator_overload;
 
+/**
+ * 新しい演算子オーバーロードを生成します.
+ * @param type
+ * @return
+ */
 operator_overload* operator_overload_new(operator_type type);
 
+/**
+ * 演算子オーバーロードを実行します.
+ * @param self
+ * @param fr
+ * @param env
+ */
 void operator_overload_execute(operator_overload* self, struct frame* fr, struct enviroment* env);
 
+/**
+ * 演算子オーバーロードを解放します.
+ * @param self
+ */
 void operator_overload_delete(operator_overload* self);
 #endif
