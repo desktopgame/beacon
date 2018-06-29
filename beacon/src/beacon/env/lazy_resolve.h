71eae2908570325b4d494375899b0264c2f777bb
Koya <koya0306@outlook.jp>
2018-06-29 23:38:11 +0900

    コメントを追加

--- beacon/src/beacon/env/lazy_resolve.h
+++ beacon/src/beacon/env/lazy_resolve.h
@@ -4,11 +4,19 @@
 struct default_eqoperator_resolve;
 struct default_noteqoperator_resolve;
 
+/**
+ * 何を遅延解決するかを表すタグ.
+ */
 typedef enum lazy_resolve_tag {
 	resolve_default_eqoperator,
 	resolve_default_noteqoperator,
 } lazy_resolve_tag;
 
+/**
+ * "遅延解決" のために一時的にキャッシュされる情報.
+ * 例えば == != の演算子オーバーロードはどんなクラスであれ自動生成されますが、
+ * これはユーザが定義していない場合に限るので遅延解決される必要があります。
+ */
 typedef struct lazy_resolve {
 	lazy_resolve_tag tag;
 	bool active;
@@ -19,9 +27,21 @@ typedef struct lazy_resolve {
 	} u;
 } lazy_resolve;
 
+/**
+ * 新しい遅延解決キャッシュを生成します.
+ * @param tag
+ */
 lazy_resolve* lazy_resolve_new(lazy_resolve_tag tag);
 
+/**
+ * 遅延解決を実行します.
+ * @param self
+ */
 void lazy_resolve_apply(lazy_resolve* self);
 
+/**
+ * 遅延解決キャッシュを解放します.
+ * @param self
+ */
 void lazy_resolve_delete(lazy_resolve* self);
 #endif
