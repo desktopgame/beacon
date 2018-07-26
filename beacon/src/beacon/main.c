52abe0b4c63fba0b6dc3f52207a79dcea45b91f9
Koya <koya0306@outlook.jp>
2018-07-26 19:19:15 +0900

    テスト失敗時にはそれが main の戻り値にも適用されるように

--- beacon/src/beacon/main.c
+++ beacon/src/beacon/main.c
@@ -32,46 +32,48 @@ int main_cl(int argc, char *argv[]) {
 	extern int optind, opterr;
 	int opt = 0;
 	int longindex = 0;
+	int ret = -1;
 	while ((opt = getopt_long(argc, argv, "ta:i:r:o:", longopts, &longindex)) != -1) {
 		switch(opt) {
 			case 't':
 				printf(":t :test\n");
-				cl_test(argc, argv);
+				ret = cl_test(argc, argv);
 				break;
 			case 'a':
 				printf(":a :ast\n");
-				cl_ast(optarg);
+				ret = cl_ast(optarg);
 				break;
 			case 'i':
 				printf(":i :il\n");
-				cl_il(optarg);
+				ret = cl_il(optarg);
 				break;
 			case 'r':
 				printf(":r :run\n");
-				cl_run(optarg);
+				ret = cl_run(optarg);
 				break;
 			case 'o':
 				printf(":o :op\n");
-				cl_op(optarg);
+				ret = cl_op(optarg);
 				break;
 			default:
 				printf("error! :%c :%c", opt, optopt);
 				break;
 		}
 	}
+	return ret;
 }
 
 int main(int argc, char *argv[]) {
 	//*
 	//先んじて設定を行っておく
-	//mem_set_trace(!true);
-	//mem_break(33221);
+	//mem_read("mem.dbg");
 	il_print_layout_hide(true);
 	//コマンドライン引数を解析して実行
 	int ret = main_cl(argc, argv);
 	//設定を元に戻す
-	//mem_dump();
-	//mem_destroy();
+	mem_write("mem.dbg");
+	mem_dump();
+	mem_destroy();
 	return ret;
 	//*/
 }
