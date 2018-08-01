d9ddc5dc190ab5fbcfe7ce07796fece273d06459
Koya <koya0306@outlook.jp>
2018-08-01 17:26:37 +0900

    リファクター

--- beacon/src/beacon/il/factor/variable/il_factor_variable_local_impl.c
+++ beacon/src/beacon/il/factor/variable/il_factor_variable_local_impl.c
@@ -10,6 +10,9 @@
 #include <string.h>
 #include <stdio.h>
 
+static void il_factor_variable_local_loadImpl(il_factor_variable_local * self, enviroment * env, call_context* cctx);
+static void il_factor_variable_local_load_field(il_factor_variable_local * self, enviroment * env, call_context* cctx);
+
 il_factor_variable_local* il_factor_variable_local_new(string_view namev) {
 	il_factor_variable_local* ret = (il_factor_variable_local*)MEM_MALLOC(sizeof(il_factor_variable_local));
 	ret->namev = namev;
@@ -33,52 +36,10 @@ void il_factor_variable_local_generate(il_factor_variable_local* self, enviromen
 }
 
 void il_factor_variable_local_load(il_factor_variable_local * self, enviroment * env, call_context* cctx) {
-	if(self->type == variable_local_undefined) {
-		//NOTE:変数宣言の後にその変数を使用する場合、
-		//factorはload時点でシンボルエントリーを取得しようとするが、
-		//stmtはgenerate時点でシンボルテーブルへ書き込むので、
-		//NULLになることがある。
-		self->type = variable_local_scope;
-		symbol_entry* ent = symbol_table_entry(env->sym_table, NULL, self->namev);
-		//ローカル変数として解決出来なかったので、
-		//フィールドとして解決する
-		if(ent == NULL) {
-			//対応するフィールドを検索
-			self->type = variable_local_field;
-			//NOTE:トップレベルではここが空なので、
-			//定義されていない変数とみなせる？
-			type* tp = call_context_type(cctx);
-			if(tp->tag == type_interface/* この条件は構文規則からして満たさないはず */) {
-				bc_error_throw(bcerror_undefined_variable, string_pool_ref2str(self->namev));
-				return;
-			}
-			int temp = -1;
-			field* f = class_find_field_tree(TYPE2CLASS(tp), self->namev, &temp);
-			self->u.field_index = temp;
-			self->type = variable_local_field;
-			if(temp == -1) {
-				bc_error_throw(bcerror_undefined_field, string_pool_ref2str(self->namev));
-				return;
-			}
-			//フィールドの型を調べる
-			generic_type* gt = f->gtype;
-			//virtual_type vt = f->vtype;
-			if(gt->tag == generic_type_tag_none) {
-				self->gt = gt;
-			} else if(gt->tag == generic_type_tag_class) {
-				self->gt = generic_type_new(NULL);
-				self->gt->tag = generic_type_tag_class;
-				self->gt->virtual_type_index = gt->virtual_type_index;
-			} else if(gt->tag == generic_type_tag_method) {
-				self->gt = generic_type_new(NULL);
-				self->gt->tag = generic_type_tag_method;
-				self->gt->virtual_type_index = gt->virtual_type_index;
-			}
-		} else {
-			self->u.entry_ = ent;
-			self->gt = ent->gtype;
-		}
+	if(self->type != variable_local_undefined) {
+		return;
 	}
+	il_factor_variable_local_loadImpl(self, env, cctx);
 }
 
 generic_type* il_factor_variable_local_eval(il_factor_variable_local * self, enviroment * env, call_context* cctx) {
@@ -95,4 +56,55 @@ void il_factor_variable_local_delete(il_factor_variable_local* self) {
 	vector_delete(self->type_args, vector_deleter_null);
 //	generic_type_delete(self->gt);
 	MEM_FREE(self);
+}
+//private
+static void il_factor_variable_local_loadImpl(il_factor_variable_local * self, enviroment * env, call_context* cctx) {
+	//NOTE:変数宣言の後にその変数を使用する場合、
+	//factorはload時点でシンボルエントリーを取得しようとするが、
+	//stmtはgenerate時点でシンボルテーブルへ書き込むので、
+	//NULLになることがある。
+	self->type = variable_local_scope;
+	symbol_entry* ent = symbol_table_entry(env->sym_table, NULL, self->namev);
+	//ローカル変数として解決出来なかったので、
+	//フィールドとして解決する
+	if(ent == NULL) {
+		il_factor_variable_local_load_field(self, env, cctx);
+	} else {
+		self->u.entry_ = ent;
+		self->gt = ent->gtype;
+	}
+}
+
+static void il_factor_variable_local_load_field(il_factor_variable_local * self, enviroment * env, call_context* cctx) {
+	//対応するフィールドを検索
+	self->type = variable_local_field;
+	//NOTE:トップレベルではここが空なので、
+	//定義されていない変数とみなせる？
+	type* tp = call_context_type(cctx);
+	if(tp->tag == type_interface/* この条件は構文規則からして満たさないはず */) {
+		bc_error_throw(bcerror_undefined_variable, string_pool_ref2str(self->namev));
+		return;
+	}
+	int temp = -1;
+	field* f = class_find_field_tree(TYPE2CLASS(tp), self->namev, &temp);
+	self->u.field_index = temp;
+	self->type = variable_local_field;
+	if(temp == -1) {
+		bc_error_throw(bcerror_undefined_field, string_pool_ref2str(self->namev));
+		return;
+	}
+	//フィールドの型を調べる
+	generic_type* gt = f->gtype;
+	//virtual_type vt = f->vtype;
+	if(gt->tag == generic_type_tag_none) {
+		self->gt = gt;
+	} else if(gt->tag == generic_type_tag_class) {
+		self->gt = generic_type_new(NULL);
+		self->gt->tag = generic_type_tag_class;
+		self->gt->virtual_type_index = gt->virtual_type_index;
+	} else if(gt->tag == generic_type_tag_method) {
+		self->gt = generic_type_new(NULL);
+		self->gt->tag = generic_type_tag_method;
+		self->gt->virtual_type_index = gt->virtual_type_index;
+	}
 }
