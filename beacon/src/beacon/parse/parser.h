5356552645463b64d409dade9a2be6f88860ad04
Koya <koya0306@outlook.jp>
2018-08-05 20:22:31 +0900

    コメントを追加

--- beacon/src/beacon/parse/parser.h
+++ beacon/src/beacon/parse/parser.h
@@ -26,21 +26,65 @@ typedef struct parser {
 	vector* lineno_vec;
 } parser;
 
+/**
+ * 文字列を入力として構文解析を実行します.
+ * 必ず parser_destroy で解放する必要があります。
+ * @param source
+ * @return
+ */
 parser* parse_string(const char* source);
 
+/**
+ * ファイルを入力として構文解析を実行します.
+ * 必ず parser_destroy で解放する必要があります。
+ * @param source
+ * @return
+ */
 parser* parse_file(const char* filename);
 
+/**
+ * 現在のパーサーを返します.
+ * @return
+ */
 parser* parser_current();
 
+/**
+ * エラー情報とASTを解放します.
+ * @param self
+ */
 void parser_destroy(parser* self);
 
+/**
+ * 文字列バッファーをクリアします.
+ * @param self
+ */
 void parser_clear_buffer(parser* self);
 
+/**
+ * 文字列バッファーに一文字加算します.
+ * @param self
+ * @return
+ */
 void parser_append_buffer(parser* self, char ch);
 
+/**
+ * 文字列バッファーをリテラルノードへ還元します.
+ * @param self
+ * @return
+ */
 ast* parser_reduce_buffer(parser* self);
 
+/**
+ * パーサーからASTを取り上げて解放させないようにします.
+ * 解析が終わった後、まだASTが必要ならこれを使用します。
+ * @param self
+ * @return
+ */
 ast* parser_release_ast(parser* self);
 
+/**
+ * エラー情報を出力します.
+ * @param p
+ */
 void parser_print_error(parser* p);
 #endif // !SIGNAL_PARSE_PARSER_H
