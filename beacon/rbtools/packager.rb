3804fd53be6ff51c9ce41bee3d0251fff8fbe11c
Koya <koya0306@outlook.jp>
2018-08-07 15:46:57 +0900

    更新履歴更新

--- beacon/rbtools/packager.rb
+++ beacon/rbtools/packager.rb
@@ -24,7 +24,7 @@ Dir.chdir("..") do
 	FileUtils.cp_r('bin/samples', 'package')
 	#マクロを展開
 	dict = {
-		"$version" => "ALPHA2",
+		"$version" => "ALPHA3",
 		"$time" => Time.now.to_s
 	}
 	#読み込みで開いて新規書き込みで書き込む。
