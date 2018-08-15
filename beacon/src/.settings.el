cf77f86fbc2db1b846fabb4bed9e7d49bdd34392
Koya <koya0306@outlook.jp>
2018-08-15 20:43:40 +0900

    いらんファイル削除

--- beacon/src/.settings.el
+++
@@ -1,8 +0,0 @@
-(when (equal major-mode 'c++-mode)
-  (setq ac-clang-cflags
-        (list (concat "-I" (expand-file-name
-                            (concat lds-dir "src")))))
-  (ac-clang-update-cmdlineargs)
-  (print ac-clang-cflags)
-)
-
