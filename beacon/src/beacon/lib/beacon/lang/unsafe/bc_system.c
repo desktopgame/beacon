b09728b1a1d3d2bd3cff73663671a8bb09284276
Koya <koya0306@outlook.jp>
2018-08-17 15:41:33 +0900

    System#exec を追加

--- beacon/src/beacon/lib/beacon/lang/unsafe/bc_system.c
+++ beacon/src/beacon/lib/beacon/lang/unsafe/bc_system.c
@@ -12,6 +12,7 @@
  */
 static void bc_system_nativeExit(method* parent, frame* fr, enviroment* env);
 static void bc_system_nativeAbort(method* parent, frame* fr, enviroment* env);
+static void bc_system_nativeExec(method* parent, frame* fr, enviroment* env);
 
 void bc_system_init() {
 	namespace_* unsafe = namespace_unsafe();
@@ -20,6 +21,7 @@ void bc_system_init() {
 	namespace_add_type(unsafe, systemType);
 	class_define_native_method(systemClass, "nativeExit", bc_system_nativeExit);
 	class_define_native_method(systemClass, "nativeAbort", bc_system_nativeAbort);
+	class_define_native_method(systemClass, "nativeExec", bc_system_nativeExec);
 }
 
 type* bc_system_type() {
@@ -38,3 +40,9 @@ static void bc_system_nativeAbort(method* parent, frame* fr, enviroment* env) {
 	abort();
 }
 
+static void bc_system_nativeExec(method* parent, frame* fr, enviroment* env) {
+	object* cmd = vector_at(fr->ref_stack, 1);
+	const char* str = bc_string_raw(cmd)->text;
+	int ret = system(str);
+	vector_push(fr->value_stack, object_int_get(ret));
+}
