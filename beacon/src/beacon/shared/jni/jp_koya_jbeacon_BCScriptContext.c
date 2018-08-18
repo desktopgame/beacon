1d0b51bb405c64bc1e226107c961f11771d829a9
Koya <koya0306@outlook.jp>
2018-08-18 15:13:47 +0900

    トップの名前空間を読み込んだ時にそのまま子要素を読むように

--- beacon/src/beacon/shared/jni/jp_koya_jbeacon_BCScriptContext.c
+++ beacon/src/beacon/shared/jni/jp_koya_jbeacon_BCScriptContext.c
@@ -1,6 +1,7 @@
 #include "jp_koya_jbeacon_BCScriptContext.h"
 #include "../../env/script_context.h"
 #include "../../env/namespace.h"
+#include <assert.h>
 
 static void bc_load_namespace(JNIEnv * env, jobject obj, numeric_map* parent);
 
@@ -64,26 +65,44 @@ static void bc_load_namespace(JNIEnv * env, jobject obj, numeric_map* parent) {
 	numeric_key key = parent->key;
 	numeric_map_item item = parent->item;
 	const char* keystr = string_pool_ref2str(key);
+	jstring keyjstr = (*env)->NewStringUTF(env, keystr);
 	namespace_* ns = (namespace_*)item;
 	//jp.koya.jbeacon.Namespace
 	jclass namespace_cls = (*env)->FindClass(env, "jp/koya/jbeacon/BCNamespace");
 	if(namespace_cls == NULL) {
 		(*env)->FatalError(env, "not found class: jp/koya/jbeacon/BCNamespace");
-		return NULL;
+		return;
 	}
 	//コンストラクタを検索
 	jmethodID namespace_ctor_id = (*env)->GetMethodID(env, namespace_cls, "<init>", "(Ljava/lang/String;[B)V");
 	if(namespace_ctor_id == NULL) {
 		(*env)->FatalError(env, "not found method: <init>");
-		return NULL;
+		return;
 	}
 	//コンストラクタを実行
 	void* buffer = (void*)ns;
 	jbyteArray pointer = (*env)->NewByteArray(env, sizeof(void*));
     (*env)->SetByteArrayRegion(env, pointer, 0, sizeof(void*), (jbyte*)&buffer);
-	jobject ret = (*env)->NewObject(env, namespace_cls, namespace_ctor_id, keystr, pointer);
+	jobject child = (*env)->NewObject(env, namespace_cls, namespace_ctor_id, keyjstr, pointer);
+	assert(child != NULL);
 	//Namespaceの持つマップへ登録する
-
+	jfieldID namespace_nmap_id = (*env)->GetFieldID(env, namespace_cls, "namespaceMap", "Ljava/util/Map;");
+	if(namespace_nmap_id == NULL) {
+		(*env)->FatalError(env, "not found field: namespaceMap");
+		return;
+	}
+	//Map#putを取得する
+	jclass map_id = (*env)->FindClass(env, "java/util/HashMap");
+	if(map_id == NULL) {
+		(*env)->FatalError(env, "not found class: HashMap");
+		return;
+	}
+	jmethodID map_put_id = (*env)->GetMethodID(env, map_id, "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
+	if(map_put_id == NULL) {
+		(*env)->FatalError(env, "not found method: put");
+		return;
+	}
+	(*env)->CallObjectMethod(env, obj, map_put_id, keyjstr, child);
 	bc_load_namespace(env, obj, parent->left);
 	bc_load_namespace(env, obj, parent->right);
 }
