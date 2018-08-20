07a8e70a482c669ce34cbb1ee341fc3b06916600
Koya <koya0306@outlook.jp>
2018-08-20 19:05:37 +0900

    コメント追加

--- beacon/src/beacon/vm/eval.h
+++ beacon/src/beacon/vm/eval.h
@@ -7,21 +7,21 @@ struct script_context;
 /**
  * 指定のファイルを解析して AST を出力します.
  * @param filename
- * @return
+ * @return 成功したなら true.
  */
 bool eval_ast(const char* filename);
 
 /**
  * 指定のファイルを解析して IL を出力します.
  * @param filename
- * @return
+ * @return 成功したなら true.
  */
 bool eval_il(const char* filename);
 
 /**
  * 指定のファイルを解析してオペコードを出力します.
  * @param filename
- * @return
+ * @return 成功したなら true.
  */
 bool eval_op(const char* filename);
 
@@ -35,7 +35,7 @@ bool eval_file(const char* filename);
 /**
  * 指定のソースコードを現在のコンテキストで実行します.
  * @param source
- * @return
+ * @return 成功したなら true.
  */
 bool eval_string(const char* source);
 #endif // !SIGNAL_VM_EVAL_H
