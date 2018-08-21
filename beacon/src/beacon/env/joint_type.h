b2f798258e1f0550ca59bc8f19dbcac47de84d85
Koya <koya0306@outlook.jp>
2018-08-21 14:47:56 +0900

    使ってないファイル削除

--- beacon/src/beacon/env/joint_type.h
+++
@@ -1,9 +0,0 @@
-#ifndef BEACON_ENV_JOIN_TYPE_H
-#define BEACON_ENV_JOIN_TYPE_H
-#include "type_interface.h"
-#include "../util/vector.h"
-
-typedef struct joint_type {
-	int pivot;
-} joint_type;
-#endif
