44b5326dece560d869bf360566e0030012e89506
Koya <koya0306@outlook.jp>
2018-08-21 09:40:33 +0900

    assign_o の命名修正

--- beacon/src/beacon/ast/assign_operator_type.h
+++ beacon/src/beacon/ast/assign_operator_type.h
@@ -5,11 +5,11 @@
  * 代入演算子の種類.
  */
 typedef enum assign_operator_type {
-	assign_otype_def,
-	assign_otype_add,
-	assign_otype_sub,
-	assign_otype_mul,
-	assign_otype_div,
-	assign_otype_mod,
+	assign_otype_def_T,
+	assign_otype_add_T,
+	assign_otype_sub_T,
+	assign_otype_mul_T,
+	assign_otype_div_T,
+	assign_otype_mod_T,
 } assign_operator_type;
 #endif // !SIGNAL_AST_ASSIGN_OPERATOR_TYPE_H
