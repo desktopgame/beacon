a7664dc6fcf12226e3868b90185060446e061e89
Koya <koya0306@outlook.jp>
2018-09-23 12:23:57 +0900

    xmlを生成するためのスクリプト追加

---
+++ beacon/rbtools/jenkins-job/generate_gcovr_xml.rb
@@ -0,0 +1,14 @@
+require "open3"
+require "fileutils"
+require "date"
+if(ARGV.length == 0) then
+    raise "few arguments"
+end
+workspace = ARGV[0]
+src = workspace + "/beacon/src"
+Dir.chdir(src) do
+    o, e, s = Open3.capture3("gcovr --xml --output=gcovr.xml ../obj")
+	p o
+	p e
+	p s
+end
