f1f1c755b31f58452b3bbda3bfdd62e23110130f
Koya <koya0306@outlook.jp>
2018-09-23 12:58:16 +0900

    gcovの実行に必要なデータを生成するように修正

--- beacon/rbtools/jenkins-job/generate_gcovr_xml.rb
+++ beacon/rbtools/jenkins-job/generate_gcovr_xml.rb
@@ -7,6 +7,14 @@ end
 workspace = ARGV[0]
 src = workspace + "/beacon/src"
 Dir.chdir(src) do
+	o, e, s = Open3.capture3("make db")
+	p o
+	p e
+	p s
+	o, e, s = Open3.capture3(workspace + "/beacon/bin/a.out --test")
+	p o
+	p e
+	p s
     o, e, s = Open3.capture3("gcovr --xml --output=gcovr.xml ../obj")
 	p o
 	p e
