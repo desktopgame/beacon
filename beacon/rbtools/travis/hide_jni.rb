527577dbb6f845a933cde05089c5ab96fdf580c5
Koya <koya0306@outlook.jp>
2018-09-23 14:29:14 +0900

    テスト時にはJNIをコンパイルしないように

---
+++ beacon/rbtools/travis/hide_jni.rb
@@ -0,0 +1,10 @@
+require "open3"
+require "fileutils"
+require "date"
+
+File.glob("beacon/src/beacon/shared/jni/*") do |file|
+    next if !file.end_with?(".h") && !file.end_with?(".c")
+    File.open(file, "w") do |fp|
+        fp.puts("/* JNIのテストは実行されません */")
+    end
+end
