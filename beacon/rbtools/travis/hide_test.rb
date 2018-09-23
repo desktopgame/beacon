e9dab1dce8cd59aed753456d34819c0e86820bb1
Koya <koya0306@outlook.jp>
2018-09-23 14:51:47 +0900

    いくつかのテストをスキップするためのスクリプトを追加

---
+++ beacon/rbtools/travis/hide_test.rb
@@ -0,0 +1,23 @@
+require "open3"
+require "fileutils"
+require "date"
+
+skip_list = ["run/main92"]
+Dir.glob("beacon/bin/semantics_test/*") do |file|
+    next if !file.end_with?(".bc")
+    path = File.expand_path(file, "beacon/bin/semantics_test")
+    #指定のファイルがスキップ一覧に入ってるかどうか
+    skip = false
+    skip_list.each do |e|
+        if path.include?(e)
+            skip = true
+            break
+        end
+    end
+    #入ってるのでスキップ
+    if skip then
+        File.open(path, "w") do |fp|
+            fp.puts(sprintf("printLine(\"skipped test %s\");", file))
+        end
+    end
+end
