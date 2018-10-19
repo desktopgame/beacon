42a68f7b0a66b5347a7f415cf77a2e8b32b7b574
Koya <koya0306@outlook.jp>
2018-10-19 21:24:23 +0900

    parse_result -> ParseResult

--- beacon/src/beacon/parse/parser.h
+++ beacon/src/beacon/parse/parser.h
@@ -11,12 +11,12 @@
 /**
  * 解析の結果を表す構造体.
  */
-typedef enum parse_result {
+typedef enum ParseResult {
 	PARSE_AWAIT_T = -1,
 	PARSE_COMPLETE_T = 0,
 	PARSE_OPEN_ERROR_T,
 	PARSE_SYNTAX_ERROR_T
-} parse_result;
+} ParseResult;
 
 /**
  * Yacc/Bisonの解析結果を保存する構造体です.
@@ -31,7 +31,7 @@ typedef struct parser {
 	int error_line_index;
 	int error_column_index;
 	yacc_input_type input_type;
-	parse_result result;
+	ParseResult result;
 	int lineno;
 	Vector* lineno_vec;
 } parser;
