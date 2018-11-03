77998679100b5cda0cd3584b4323a5984e7a00da
Koya <koya0306@outlook.jp>
2018-11-03 16:10:29 +0900

    リファクタリング

--- beacon/src/beacon/vm/frame.c
+++ beacon/src/beacon/vm/frame.c
@@ -8,10 +8,10 @@
 
 //proto
 static void remove_from_parent(Frame* self);
-static void frame_markStatic(Field* item);
-static void frame_markRecursive(Frame* self);
+static void mark_static(Field* item);
+static void mark_recursive(Frame* self);
 static void frame_mark_defer(Frame* self);
-static void DeleteFrame_defctx(VectorItem e);
+static void delete_defctx(VectorItem e);
 
 Frame* NewFrame() {
 	Frame* ret = (Frame*)MEM_MALLOC(sizeof(Frame));
@@ -44,9 +44,9 @@ Frame* SubFrame(Frame* parent) {
 void MarkAllFrame(Frame* self) {
 	//全ての静的フィールドをマークする
 	ScriptContext* ctx = GetCurrentScriptContext();
-	EachStaticScriptContext(ctx, frame_markStatic);
+	EachStaticScriptContext(ctx, mark_static);
 	//全ての子要素を巡回してマーキング
-	frame_markRecursive(self);
+	mark_recursive(self);
 }
 
 void DeleteFrame(Frame* self) {
@@ -58,7 +58,7 @@ void DeleteFrame(Frame* self) {
 	DeleteVector(self->VariableTable, VectorDeleterOfNull);
 	DeleteVector(self->Children, VectorDeleterOfNull);
 	DeleteVector(self->TypeArgs, VectorDeleterOfNull);
-	DeleteVector(self->DeferList, DeleteFrame_defctx);
+	DeleteVector(self->DeferList, delete_defctx);
 	MEM_FREE(self);
 }
 
@@ -74,7 +74,7 @@ static void remove_from_parent(Frame* self) {
 	}
 }
 
-static void frame_markStatic(Field* item) {
+static void mark_static(Field* item) {
 	//フィールドがintなどならここでマークしない
 	//静的定数フィールドに初期値が割り当てられていない場合
 	if(item->StaticValue != NULL &&
@@ -83,10 +83,10 @@ static void frame_markStatic(Field* item) {
 	}
 }
 
-static void frame_markRecursive(Frame* self) {
+static void mark_recursive(Frame* self) {
 	for (int i = 0; i < self->Children->Length; i++) {
 		Frame* e = (Frame*)AtVector(self->Children, i);
-		frame_markRecursive(e);
+		mark_recursive(e);
 	}
 	for (int i = 0; i < self->ValueStack->Length; i++) {
 		Object* e = (Object*)AtVector(self->ValueStack, i);
@@ -115,7 +115,7 @@ static void frame_mark_defer(Frame* self) {
 		}
 	}
 }
-static void DeleteFrame_defctx(VectorItem e) {
+static void delete_defctx(VectorItem e) {
 	DeferContext* defctx = (DeferContext*)e;
 	DeleteDeferContext(defctx);
 }
