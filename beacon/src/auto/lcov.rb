670467d730d41add30cad4028d933d83542084bc
Koya <koya0306@outlook.jp>
2018-11-11 20:32:15 +0900

    lcov.rbを更新

--- beacon/src/auto/lcov.rb
+++ beacon/src/auto/lcov.rb
@@ -2,16 +2,13 @@ require "open3"
 require "fileutils"
 require "date"
 #makeを実行する
-o, e, s = Open3.capture3("make db")
+o, e, s = Open3.capture3("make")
 p e
 p
 #テストを実行する
-Dir.chdir("../bin") do
-    o, e, s = Open3.capture3("./a.out --test")
-    o.lines {|line| p "    " + line}
-end
+o, e, s = Open3.capture3("make test ARGS='-V'")
+o.lines {|line| p "    " + line}
 #.gcda .gcno をコピー
-FileUtils.cp_r("../obj/beacon", ".")
 File.delete("lcov.info") if File.exists?("lcov.info")
 #lcov.info生成
 o, e, s = Open3.capture3("lcov -c -d . -o lcov.info")
@@ -20,13 +17,5 @@ p
 #html生成
 o, e, s = Open3.capture3("genhtml -o lcov lcov.info")
 p e
-#掃除
-Dir.glob("**/*") do |file|
-    next if (!file.end_with?(".gcda") &&
-             !file.end_with?(".gcno") &&
-             !file.end_with?(".o") &&
-             !file.end_with?(".d"))
-    File.delete(file)
-end
 page = sprintf("chrome %s", "file://" + File.expand_path("./lcov/index.html"))
 p page
