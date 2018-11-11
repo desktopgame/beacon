9ffdaa6d1bcfb10597e933c4fe78cb80af3565dd
Koya <koya0306@outlook.jp>
2018-11-11 18:41:21 +0900

    CMakeのための設定

---
+++ beacon/src/auto/cmake.rb
@@ -0,0 +1,115 @@
+require "open3"
+require "fileutils"
+require "date"
+require 'fileutils'
+=begin
+ユーザ側の設定例
+.bash_profile
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+
+makefile作成の例
+cmake -D JNI_INCLUDE_DIR=/Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home/include -D JNI_NATIVE_INCLUDE_DIR=/Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home/include/darwin .
+=end
+
+@template = <<'EOS'
+cmake_minimum_required(VERSION 2.8)
+project(beacon C)
+#あなたのインクルードパスを設定
+set(CMAKE_CXX_COMPILER clang)
+set(JNI_INCLUDE_DIR "" CACHE FILEPATH "Path to jni")
+set(JNI_NATIVE_INCLUDE_DIR "" CACHE FILEPATH "Path to jni native")
+include_directories(${JNI_INCLUDE_DIR} ${JNI_NATIVE_INCLUDE_DIR})
+EOS
+@all = []
+
+def path_to_bar(path)
+    path = path.gsub(Dir.pwd, "lib_")
+    path.gsub("/", "_")
+end
+
+def create_cmake(cmake_path, dir, files, depth)
+    File.open(cmake_path, "w") do |fp|
+        fp.puts(@template) if depth == 0
+        files.each do |e|
+            if FileTest.directory?(e) then
+                fp.puts(sprintf("add_subdirectory(\"%s\")", File.basename(e)))
+            end
+        end
+        #ライブラリをビルド
+        if depth > 0 then
+            lib = sprintf("add_library(\"%s\" STATIC\n", (path_to_bar(dir)))
+            c_files = files.select {|e| e.end_with?(".c") }
+            c_files.each {|e| lib << sprintf("    \"%s\"\n", e.gsub(dir + "/", ""))}
+            lib << ")\n"
+            #ライブラリ一覧に追加
+            if c_files.length > 0 then
+                fp.puts(lib)
+                @all << path_to_bar(dir)
+            end
+        end
+        #実行ファイルをビルド
+        if depth == 0 then
+            exe = sprintf("add_executable(beacon_exe \"beacon/main.c\")")
+            fp.puts(exe)
+            #ライブラリのリンク
+            link = sprintf("target_link_libraries(beacon_exe\n")
+            @all.each {|e| link << sprintf("    \"%s\"\n", e)}
+            link << ")\n"
+            fp.puts(link)
+            @all = []
+        end
+    end
+end
+
+def each_recursive(path, depth)
+    file_list = []
+    #パスを展開する
+    opath = path
+    path = File.expand_path(path)
+    cmake_path = path + "/CMakeLists.txt"
+    cache_path = path + "/CMakeCache.txt"
+    cmake_dir = path + "/CMakeFiles"
+    insta_path = path + "/cmake_install.cmake"
+    makef_path = path + "/Makefile"
+    puts ("open: " + path)
+    puts ("make: " + cmake_path)
+    #CMakeLists.txtが存在するなら削除する
+    if File.exist?(cmake_path) then
+        FileUtils.rm(cmake_path)
+    end
+    #CMakeFilesが存在するなら削除する
+    if File.exist?(cmake_dir) then
+        FileUtils.rm_r(cmake_dir)
+    end
+    #CMakeCacheが存在するなら削除する
+    if File.exist?(cache_path) then
+        FileUtils.rm(cache_path)
+    end
+    #installが存在するなら削除する
+    if File.exist?(insta_path) then
+        FileUtils.rm(insta_path)
+    end
+    #makefileが存在するなら削除する
+    if File.exist?(makef_path) then
+        FileUtils.rm(makef_path)
+    end
+    #全てのファイルを開く
+    Dir.open(path).each do |file|
+        next if(file == "." || file == "..")
+        next if(file == "lcov" || file == "auto" || file == "script")
+        file = path + "/" + file
+        puts ("  " + file)
+        if(FileTest.directory?(file)) then
+            each_recursive(file, depth + 1)
+            file_list << file
+        #ファイルを登録する
+        else
+            file_list << file
+        end
+    end
+    create_cmake(cmake_path, path, file_list, depth)
+    file_list
+end
+
+each_recursive(".", 0)
