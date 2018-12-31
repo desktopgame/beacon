01ce5c297ca68bb73493a08085a69e67ec1c514a
Koya <koya0306@outlook.jp>
2018-12-31 21:21:23 +0900

    floatリテラルのIL表現を追加

---
+++ beacon/src/beacon/il/factor/il_factor_float_impl.c
@@ -0,0 +1,49 @@
+#include "il_factor_float_impl.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../env/TYPE_IMPL.h"
+#include "../../env/namespace.h"
+#include "../../util/mem.h"
+#include "../../util/text.h"
+#include "../../vm/enviroment.h"
+#if defined(_MSC_VER)
+#pragma warning(disable : 4996)
+#endif
+
+bc_ILFactor* bc_WrapILFloat(bc_ILFloat* self) {
+        bc_ILFactor* ret = bc_NewILFactor(ILFACTOR_FLOAT_T);
+        ret->Kind.Float = self;
+        return ret;
+}
+
+bc_ILFloat* bc_NewILFloat(double d) {
+        bc_ILFloat* ret = (bc_ILFloat*)MEM_MALLOC(sizeof(bc_ILFloat));
+        ret->Value = d;
+        return ret;
+}
+
+void bc_GenerateILFloat(bc_ILFloat* self, bc_Enviroment* env,
+                        bc_CallContext* cctx) {
+        int index = bc_AddCFloatEnviroment(env, self->Value);
+        bc_AddOpcodeBuf(env->Bytecode, OP_DCONST);
+        bc_AddOpcodeBuf(env->Bytecode, index);
+}
+
+void bc_LoadILFloat(bc_ILFloat* self, bc_Enviroment* env,
+                    bc_CallContext* cctx) {}
+
+bc_GenericType* bc_EvalILFloat(bc_ILFloat* self, bc_Enviroment* env,
+                               bc_CallContext* cctx) {
+        return BC_GENERIC_FLOAT;
+}
+
+char* bc_ILFloatToString(bc_ILFloat* self, bc_Enviroment* env) {
+        bc_Buffer* sb = bc_NewBuffer();
+        char block[32] = {0};
+        int res = sprintf(block, "%lf", self->Value);
+        assert(res >= 0);
+        bc_AppendsBuffer(sb, block);
+        return bc_ReleaseBuffer(sb);
+}
+
+void bc_DeleteILFloat(bc_ILFloat* self) { MEM_FREE(self); }
